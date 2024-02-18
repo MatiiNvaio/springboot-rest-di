3b355888b3c05dc991f83c16d5d49f859eadf2f7
Matias Navio <naviomatias2@gmail.com>
2024-02-18 14:55:57 -0300

    Initial commit

---
+++ src/main/java/com/matias/springboot/di/app/springbootrestdi/SpringbootRestDiApplication.java
@@ -0,0 +1,13 @@
+package com.matias.springboot.di.app.springbootrestdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestDiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matias/springboot/di/app/springbootrestdi/SpringbootRestDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matias.springboot.di.app.springbootrestdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
